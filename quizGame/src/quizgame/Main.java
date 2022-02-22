7df64fedb7196946be8d8448e0b74a75fc97934e
misba0770 <80534061+misba0770@users.noreply.github.com>
2022-02-22 18:57:26 +0530

    Add files via upload

---
+++ quizGame/src/quizgame/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizgame;
+
+/**
+ *
+ * @author misba zaidi
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        QuizGame quiz=new QuizGame();
+    }
+    
+}
